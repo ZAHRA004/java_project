8accc7af498033b62e62322ed78fdef70c6a8e3c
hasti <hnajafi1383@gmail.com>
2023-06-21 08:47:20 -0700

    reportplant

--- src/finalClass/Menu.java
+++ src/finalClass/Menu.java
@@ -45,13 +45,13 @@ public class Menu {
 	public static void record() {
 		
 		System.out.println("Selact the desired option : ");
-		System.out.println("1.alive creature\n2.natural resource \n>>>>>");
+		System.out.println("1.alive creature\n2.natural resource\n3.Back\n>>>>>");
 		int x = console.nextInt();
 		switch (x) {
 		case 1 :
 		{
 			System.out.println("Selact the desired option : ");
-			System.out.println("1.human\n2.animal\n>>>>>");
+			System.out.println("1.human\n2.animal\n3.Back\\n>>>>>");
 			int y = console.nextInt();
 			switch (y) {
 			case 1 :
@@ -60,6 +60,8 @@ public class Menu {
 			case 2 :
 				animal();
 				break ;
+			case 3 : record();
+			break;
 			default :
 			{
 				System.out.println("The desired option is not allowed!!!");
@@ -72,7 +74,7 @@ public class Menu {
 		case 2 :
 		{
 			System.out.println("Selact the desired option : ");
-			System.out.println("1.terrene\n2.plant\n>>>>>");
+			System.out.println("1.terrene\n2.plant\n3.back\n>>>>>");
 			int z = console.nextInt();
 			switch (z) {
 			case 1 :
@@ -81,6 +83,8 @@ public class Menu {
 			case 2 :
 				plant() ;
 				break ;
+			case 3 : record();
+			break;
 			default :
 			{
 				System.out.println("The desired option is not allowed!!!");
@@ -90,6 +94,8 @@ public class Menu {
 			}
 			break ;
 		}
+		case 3 : menu();
+		break;
 		default :
 		{
 			System.out.println("The desired option is not allowed!!!");
@@ -102,7 +108,7 @@ public class Menu {
 	public static void human() {
 		
 		System.out.println("Selact the desired option : ");
-		System.out.println("1.fars\n2.arab\n3.tork\n4.kord\n5.baloch\n6.torkaman\n>>>>>");
+		System.out.println("1.fars\n2.arab\n3.tork\n4.kord\n5.baloch\n6.torkaman\n7.back\n>>>>>");
 		int k = console.nextInt();
 		switch (k) {
 		case 1 :
@@ -113,6 +119,8 @@ public class Menu {
 		case 6 :
 			person(k);
 			break ;
+		case 7 : record();
+		break;
 		default :
 		{
 			System.out.println("The desired option is not allowed!!!");
@@ -125,13 +133,15 @@ public class Menu {
 	public static void animal() {
 		
 		System.out.println("Selact the desired option : ");
-		System.out.println("1.mamal\n2.oviparous\n>>>>>");
+		System.out.println("1.mamal\n2.oviparous\n3.back\n>>>>>");
 		int i = console.nextInt();
 		switch (i) {
 		case 1 :
 		case 2 :
 			animalInfo(i);
 			break ;
+		case 3 : record();
+			break;
 		default :
 		{
 			System.out.println("The desired option is not allowed!!!");
@@ -144,13 +154,13 @@ public class Menu {
 	public static void terrene() {
 		
 		System.out.println("Selact the desired option : ");
-		System.out.println("1.water area\n2.land\n>>>>>");
+		System.out.println("1.water area\n2.land\n3.back\n>>>>>");
 		int j = console.nextInt();
 		switch (j) {
 		case 1 :
 		{
 			System.out.println("Selact the desired option : ");
-			System.out.println("1.lake\n2.sea\n>>>>>");
+			System.out.println("1.lake\n2.sea\n3.back\n>>>>>");
 			int k = console.nextInt();
 			switch (k) {
 			case 1 :
@@ -159,6 +169,8 @@ public class Menu {
 				waterArea(k);
 				break ;
 			}
+			case 3 : terrene();
+			break;
 			default :
 			{
 				System.out.println("The desired option is not allowed!!!");
@@ -171,7 +183,7 @@ public class Menu {
 		case 2 :
 		{
 			System.out.println("Selact the desired option : ");
-			System.out.println("1.desert\n2.plain\n3.mountain\n4.forest\n>>>>>");
+			System.out.println("1.desert\n2.plain\n3.mountain\n4.forest\n5.back\n>>>>>");
 			int k = console.nextInt();
 			switch (k) {
 			case 1 :
@@ -186,6 +198,8 @@ public class Menu {
 				mountain();
 				break ;
 			}
+			case 5 : terrene() ;
+			break;
 			default :
 			{
 				System.out.println("The desired option is not allowed!!!");
@@ -195,6 +209,8 @@ public class Menu {
 			}
 			break ;
 		}
+		case 3 : record();
+			break;
 		default :
 		{
 			System.out.println("The desired option is not allowed!!!");
@@ -208,13 +224,13 @@ public class Menu {
 	public static void plant() {
 		
 		System.out.println("Selact the desired option : ");
-		System.out.println("1.tree\n2.flower\n3.medical\n4.feedstuf\n>>>>>");
+		System.out.println("1.tree\n2.flower\n3.medical\n4.feedstuf\n5.back\n>>>>>");
 		int m = console.nextInt();
 		switch (m) {
 		case 1 :
 		{
 			System.out.println("Selact the desired option : ");
-			System.out.println("1.Fruit\n2.Fruitless\n>>>>>");
+			System.out.println("1.Fruit\n2.Fruitless\n3.back\n>>>>>");
 			int i = console.nextInt();
 			switch (i) {
 			case 1 :
@@ -227,6 +243,8 @@ public class Menu {
 				m = 6 ;
 				break ;
 			}
+			case 3 : plant();
+			break;
 			default :
 			{
 				System.out.println("The desired option is not allowed!!!");
@@ -242,6 +260,8 @@ public class Menu {
 		case 4 :
 			plantInfo(m);
 			break ;
+		case 5 : record();
+			break;
 		default :
 		{
 			System.out.println("The desired option is not allowed!!!");
@@ -1125,11 +1145,14 @@ public class Menu {
 			break;
 			case 2 : numanimalres();
 			break;
-			case 3 :
+			case 3 : numplantres();
 			break;
 			case 4 :
 			break;
 			case 5 : menu();
+			break;
+			default: System.out.printf("incorecct\n");
+			        report();
 		}
 
 		
@@ -1260,15 +1283,17 @@ public class Menu {
             }
 			 reporthuman.close();
 			 
-			System.out.printf("qhom    mard     zan    kol \n");
-			System.out.printf("tork      %d      %d    %d \n", ctorkmale , ctorkfemale , ctork);
-			System.out.printf("kord      %d      %d    %d \n", ckordmale , ckordfemale , ckord);
-			System.out.printf("fars      %d      %d    %d \n", cfarsmale , cfarsfemale , cfars);
-			System.out.printf("arab      %d      %d    %d \n", carabmale , carabfemale , carab);
-			System.out.printf("torkaman  %d      %d    %d \n", ctorkamanmale , ctorkamanfemale , ctorkaman);
-			System.out.printf("baloch    %d      %d    %d \n", cbalochmale , cbalochfemale , cbaloch);
+			System.out.printf("qhom    mard    zan    kol \n");
+			System.out.printf("tork      %d      %d     %d \n", ctorkmale , ctorkfemale , ctork);
+			System.out.printf("kord      %d      %d     %d \n", ckordmale , ckordfemale , ckord);
+			System.out.printf("fars      %d      %d     %d \n", cfarsmale , cfarsfemale , cfars);
+			System.out.printf("arab      %d      %d     %d \n", carabmale , carabfemale , carab);
+			System.out.printf("torkaman  %d      %d     %d \n", ctorkamanmale , ctorkamanfemale , ctorkaman);
+			System.out.printf("baloch    %d      %d     %d \n", cbalochmale , cbalochfemale , cbaloch);
 			System.out.printf("kol jamiat : %d nafar\n", (ctork + ckord + cfars + carab + cbaloch + ctorkaman));
 			System.out.printf("__________________________________ \n");
+			int o = 0;
+            while(o == 0){
 			System.out.printf("\n1.Details by ethnicity \n");
 			System.out.printf("2.Details by gender \n");
 			System.out.printf("3.Back \n");
@@ -1276,7 +1301,7 @@ public class Menu {
 		    int x = console.nextInt();
 			switch(x)
 			{
-				case 1 : 
+				case 1 : while(o == 0){
 				System.out.println("1.fars\n2.arab\n3.tork\n4.kord\n5.baloch\n6.torkaman\n7.Back\n");
 				System.out.printf("Enter the desired number : \n");
 				x = console.nextInt();
@@ -1286,8 +1311,9 @@ public class Menu {
 					else{numhumanres1(x);}
 				}
 				else{System.out.printf("incorecct\n");}
+			    }
 				break;
-				case 2 :
+				case 2 : while( o == 0){
 				System.out.println("1.male\n2.female\n3.Back\n");
 				System.out.printf("Enter the desired number : \n");
 				x = console.nextInt();
@@ -1297,11 +1323,14 @@ public class Menu {
 					else{numhumanres2(x);}
 				}
 				else{System.out.printf("incorecct\n");}
+			    }
 				break;
 				case 3 : report();
 				break;
 				default:System.out.printf("incorecct\n");
+				        
 			}
+		    }
 
 
 
@@ -1491,7 +1520,18 @@ public class Menu {
 		}
 		reportbygh.close();
 		System.out.printf("\n");
-		numhumanres();
+		int o = 0;
+		while(o == 0){
+		System.out.printf("1.back\n");
+		System.out.printf("Enter the desired number : \n");
+		x = console.nextInt();
+		if(x == 1){
+			numhumanres();
+		}
+		else{
+			System.out.printf("incorecct\n");
+		}
+	    }
 
 		} catch(IOException e) {
             e.printStackTrace();
@@ -1570,9 +1610,19 @@ public class Menu {
 		}
 		reportbyge.close();
 		System.out.printf("\n");
-		numhumanres();
-
-			
+		int o = 0;
+		while(o == 0){
+		System.out.printf("1.back\n");
+		System.out.printf("Enter the desired number : \n");
+		x = console.nextInt();
+		if(x == 1){
+			numhumanres();
+		}
+		else{
+			System.out.printf("incorecct\n");
+		}
+	    }
+		
 		} catch(IOException e) {
             e.printStackTrace();
 		}
@@ -1581,7 +1631,306 @@ public class Menu {
 
 	public static void numanimalres()
 	{
+		try {
+			FileReader numanimal = new FileReader("animal.txt");
+
+            BufferedReader reportanimal = new BufferedReader(numanimal);
+
+			int cmamal = 0 , coviparous = 0;
+
+			String line = null;
+
+            while( (line = reportanimal.readLine() ) != null ) {
+				int check = line.compareTo("mamal");
+                if( check == 0)
+				{
+					cmamal++ ;
+				}
+				else
+				{
+					
+					check = line.compareTo("oviparous");
+                    if( check == 0)
+				    {
+					    coviparous++ ;
+				    }
+				}
+			}
+			reportanimal.close();
+
+		    System.out.printf("type       number \n");
+			System.out.printf("mamal        %d     \n", cmamal);
+			System.out.printf("oviparous    %d     \n\n", coviparous);
+			System.out.printf("total : %d \n", coviparous + cmamal);
+			System.out.printf("_______________________________ \n\n");
+			int o = 0;
+			while( o == 0){
+			System.out.printf("1.Details of special animals \n");
+			System.out.printf("2.Back \n");
+			System.out.printf("Enter the desired number : \n");
+		    int x = console.nextInt();
+			switch(x)
+			{
+				case 1 : numanimalres1();
+				break;
+				case 2 : report();
+				break;
+				default : System.out.printf("incorecct\n");
+			}
+			}
+			
+		} catch(IOException e) {
+            e.printStackTrace();
+		}
 		
 	}
+
+	public static void numanimalres1()
+	{
+		try {
+			FileReader numanimal = new FileReader("animal.txt");
+
+            BufferedReader reportanimalsp = new BufferedReader(numanimal);
+
+			String line = null, id = "id", type = "type" , name = "name" ;
+			System.out.printf("%s%18s%10s\n", id,type,name);
+			int c = 0;
+
+			while( (line = reportanimalsp.readLine() ) != null) 
+			{
+				int chack = line.compareTo("0");
+				if(chack != 0)
+				{
+					c++;
+					System.out.printf("%10s", line);
+					for(int i = 0 ; i< 2 ; i++)
+					{
+						line = reportanimalsp.readLine();
+						System.out.printf("%10s", line);
+					}
+					line = reportanimalsp.readLine();
+					System.out.printf("\n");
+
+				}
+				else
+				{
+					for(int i = 0 ; i< 3 ; i++)
+					{
+						line = reportanimalsp.readLine();
+					}
+				}
+				
+			}
+			System.out.printf("total : %d \n", c);
+			int o = 0;
+			while(o == 0){
+		    System.out.printf("1.back\n");
+		    System.out.printf("Enter the desired number : \n");
+		    int x = console.nextInt();
+		    if(x == 1){
+			    numanimalres();
+		    }
+		    else{
+			    System.out.printf("incorecct\n");
+		    }
+	        }
+			reportanimalsp.close();
+
+		} catch(IOException e) {
+            e.printStackTrace();
+		}
+
+	}
+
+	public static void numplantres()
+	{
+		try {
+			FileReader numplant = new FileReader("plant.txt");
+
+            BufferedReader reportplant = new BufferedReader(numplant);
+
+			int cfruit = 0 , cfruitless = 0 , cflower = 0 , cmedical = 0 ,cfeedstuf = 0 , temp = 0 ;
+			
+
+			String line = null;
+
+            while( (line = reportplant.readLine() ) != null ) {
+				int check = line.compareTo("fruit");
+                if( check == 0)
+				{
+					temp = Integer.parseInt(reportplant.readLine());
+					cfruit = cfruit + temp ;
+				}
+				else
+				{
+					
+					check = line.compareTo("fruitLess");
+                    if( check == 0)
+				    {
+					    temp = Integer.parseInt(reportplant.readLine());
+					    cfruitless = cfruitless + temp ;
+				    }
+					else
+					{
+						check = line.compareTo("flower");
+						if( check == 0)
+				        {
+					        temp = Integer.parseInt(reportplant.readLine());
+					        cflower = cflower + temp ;
+				        }
+						else
+						{
+							check = line.compareTo("medical");
+						    if( check == 0)
+				            {
+					            temp = Integer.parseInt(reportplant.readLine());
+					            cmedical = cmedical + temp ;
+				            }
+							else
+							{
+								check = line.compareTo("feedStuf");
+						        if( check == 0)
+				                {
+					                temp = Integer.parseInt(reportplant.readLine());
+					                cfeedstuf = cfeedstuf + temp ;
+				                }
+							}
+
+						}
+					}
+				}
+			}
+			reportplant.close();
+
+		    System.out.printf("type          number \n");
+			System.out.printf("fruitTree        %d     \n", cfruit);
+			System.out.printf("fruitlessTree    %d     \n", cfruitless ) ;
+			System.out.printf("flower           %d     \n", cflower);
+			System.out.printf("medical          %d     \n", cmedical);
+			System.out.printf("feedStuf         %d     \n\n", cfeedstuf);
+			System.out.printf("total : %d \n", cfruit + cfruitless + cflower + cmedical + cfeedstuf);
+			System.out.printf("_______________________________ \n\n");
+			int o = 0;
+			while( o == 0){
+			System.out.printf("1.Details of trees \n");
+			System.out.printf("2.Details of flowers \n");
+			System.out.printf("3.Details of medical \n");
+			System.out.printf("4.Details of feedStuf \n");
+			System.out.printf("5.Back \n");
+			System.out.printf("Enter the desired number : \n");
+		    int x = console.nextInt();
+			switch(x)
+			{
+				case 1 : 
+				case 2 :
+				case 3 :
+				case 4 : numplantres1(x);
+				break;
+				case 5 : report();
+				break;
+				default : System.out.printf("incorecct\n");
+			}
+			}
+			
+		} catch(IOException e) {
+            e.printStackTrace();
+		}
+
+	}
+
+	public static void numplantres1 (int x )
+	{
+		try {
+			FileReader numplant = new FileReader("plant.txt");
+
+            BufferedReader reportplant1 = new BufferedReader(numplant);
+			String line1 = null , line2 = null , line3=null , type = "type" , name = "name" , number = "number" ;
+			int check ;
+			if(x == 1)
+		    {
+			System.out.printf("%s%12s%20s\n", name , type , number);
+
+			while((line1 = reportplant1.readLine()) != null )
+			{
+				check = line1.compareTo("fruit");
+                if( check == 0)
+				{
+					String fruit = "fruitTree";
+					line2 = reportplant1.readLine();
+					line3 = reportplant1.readLine();
+					System.out.printf("%s%17s%15s\n", line3 ,fruit , line2);
+					continue;
+				}
+				check = line1.compareTo("fruitLess");
+                 if( check == 0)
+				{
+					String fruit = "fruitlessTree";
+					line2 = reportplant1.readLine();
+					line3 = reportplant1.readLine();
+					System.out.printf("%s%20s%10s\n", line3 ,fruit , line2);
+				}
+
+			}
+		    }
+			if(x == 2)
+			{
+				System.out.printf("%s%10s\n", name , number);
+				while((line1 = reportplant1.readLine()) != null )
+				{
+					check = line1.compareTo("flower");
+                    if( check == 0)
+					{
+						line1 = reportplant1.readLine();
+						line2 = reportplant1.readLine();
+						System.out.printf("%s%10s\n", line2 , line1);
+					}
+				}
+
+			}
+			if(x == 3)
+			{
+				System.out.printf("%s%10s\n", name , number);
+				while((line1 = reportplant1.readLine()) != null )
+				{
+					check = line1.compareTo("medical");
+                    if( check == 0)
+					{
+						line1 = reportplant1.readLine();
+						line2 = reportplant1.readLine();
+						System.out.printf("%s%10s\n", line2 , line1);
+					}
+				}
+			}
+			if(x==4)
+			{
+				System.out.printf("%s%10s\n", name , number);
+				while((line1 = reportplant1.readLine()) != null )
+				{
+					check = line1.compareTo("feedStuf");
+                    if( check == 0)
+					{
+						line1 = reportplant1.readLine();
+						line2 = reportplant1.readLine();
+						System.out.printf("%s%10s\n", line2 , line1);
+					}
+				}
+			}
+			reportplant1.close();
+			int c = 0 ;
+            while(c == 0){
+			System.out.printf("1.Back \n");
+			System.out.printf("Enter the desired number : \n");
+		    int a = console.nextInt();
+			if(a != 1)
+			{System.out.printf("incorecct\n");}
+			else{c = 1;}
+			}
+		} catch(IOException e) {
+            e.printStackTrace();
+		}
+
+	}
+
+	
     
 }
